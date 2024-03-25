5e43e8c9081022d91a4a5e45799a13eb5ec231e8
PraiseJones <praisejones18@gmail.com>
2024-03-25 20:08:21 +0100

    service section

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,33 @@
 const checkbox = document.querySelector("#check");
-const ddIcon = document.querySelector("#dd-icon");
+const openIcon = document.querySelectorAll("#dd-icon");
 const closeIcon = document.querySelector("#close-icon");
+
+const contScroll = document.querySelector('.scroller');
+var isMouseDown = false;
+var startX, scrollLeft;
+
+contScroll.addEventListener('mousedown', (e) => {
+  isMouseDown = true;
+  startX = e.pageX - contScroll.offsetLeft;
+  scrollLeft = contScroll.scrollLeft;
+  // console.log('something');
+});
+
+document.addEventListener('mouseup', ()=> {
+  isMouseDown = false;
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isMouseDown) return;
+  const x = e.pageX-contScroll.offsetLeft;
+  const scroll = x-startX;
+  contScroll.scrollLeft = scrollLeft-scroll;
+});
+
+contScroll.addEventListener('wheel', (e) => {
+  e.preventDefault();
+  contScroll.scrollLeft += e.deltaX
+})
 
 closeIcon.addEventListener("click", () => {
   const menu = document.querySelector('.menu');
@@ -8,8 +35,24 @@ closeIcon.addEventListener("click", () => {
   menu.classList.add('top-full');
 });
 
-ddIcon.addEventListener("click", () => {
+openIcon.addEventListener("click", () => {
   const menu = document.querySelector('.menu');
   menu.classList.remove('top-full');
   menu.classList.add('top-0');
 });
+
+//scroll functionality
+
+// const contScroll = document.querySelector('.scroller');
+// var isMouseDown = false;
+// var startX, scrollLeft;
+
+// console.log(contScroll, ":is amazing");
+
+// contScroll.addEventListener('mousedown', (e) => {
+//   isMouseDown = true;
+//   startX = e.pageX - contScroll.offsetLeft;
+//   scrollLeft = contScroll.scrollLeft;
+// });
+
+// document.addEventListener('mouseup', ())
